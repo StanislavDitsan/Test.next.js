4707d906b874fdda70e90621a2eec1a6b634de0f
StanislavDitsan <ditsanstas@gmail.com>
2024-04-19 04:49:45 +0200

    Using custom colors in tailwind and custom css

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
+const posts = [
+    {
+        id: 1,
+        title: 'Boost your conversion rate',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+        date: 'Mar 16, 2020',
+        datetime: '2020-03-16',
+        category: { title: 'Marketing', href: '#' },
+        author: {
+            name: 'Michael Foster',
+            role: 'Co-Founder / CTO',
+            href: '#',
+            imageUrl:
+                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+    },
+    // More posts...
+]
+
 export default function Blog() {
     return (
         <div className="min-h-screen bg-gradient-to-b from-blue-200 to-blue-400 flex justify-center items-center mt-2">
@@ -9,7 +30,7 @@ export default function Blog() {
                     Blog </p>
                 <a
                     href="/about"
-                    className="bg-white text-gray-800 font-semibold py-2 px-4 rounded hover:bg-gray-200 transition duration-300"
+                    className="bg-white text-primary font-semibold py-2 px-4 rounded hover:bg-gray-200 transition duration-300"
                 >
                     More
                 </a>
@@ -17,3 +38,4 @@ export default function Blog() {
         </div>
     );
 }
+
